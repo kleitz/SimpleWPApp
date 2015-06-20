8ce1efc11433d03dad423a617814ffdf06af5f81
OrangeFlash81 <aaronc20000@gmail.com>
2015-06-20 10:44:53 +0100

    First commit

---
+++ App1/BasicPage2.xaml.vb
@@ -0,0 +1,41 @@
+﻿Imports App1.Common
+Public NotInheritable Class BasicPage2
+    Inherits Page
+
+    Private WithEvents _navigationHelper As New NavigationHelper(Me)
+    Private ReadOnly _defaultViewModel As New ObservableDictionary()
+    Public ReadOnly Property NavigationHelper As NavigationHelper
+        Get
+            Return _navigationHelper
+        End Get
+    End Property
+
+    Sub New()
+        InitializeComponent()
+        Me._navigationHelper = New Common.NavigationHelper(Me)
+        AddHandler Me._navigationHelper.LoadState, AddressOf NavigationHelper_LoadState
+        AddHandler Me._navigationHelper.SaveState, AddressOf NavigationHelper_SaveState
+    End Sub
+
+    Public ReadOnly Property DefaultViewModel As ObservableDictionary
+        Get
+            Return _defaultViewModel
+        End Get
+    End Property
+    Private Sub NavigationHelper_LoadState(sender As Object, e As LoadStateEventArgs) Handles _navigationHelper.LoadState    End Sub
+
+    Private Sub NavigationHelper_SaveState(sender As Object, e As SaveStateEventArgs) Handles _navigationHelper.SaveState    End Sub
+
+#Region "NavigationHelper registration"
+
+    Protected Overrides Sub OnNavigatedTo(e As NavigationEventArgs)
+        _navigationHelper.OnNavigatedTo(e)
+    End Sub
+
+    Protected Overrides Sub OnNavigatedFrom(e As NavigationEventArgs)
+        _navigationHelper.OnNavigatedFrom(e)
+    End Sub
+
+#End Region
+
+End Class
